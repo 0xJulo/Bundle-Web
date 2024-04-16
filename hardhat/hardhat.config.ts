33c3ca334877361ff009bf645ac0c87706c0d84f
alerex.eth <alessandromaci96@gmail.com>
2024-04-16 20:00:23 +0200

    deploy bundle contract on arbitrum sepolia

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,32 @@
 import { HardhatUserConfig } from "hardhat/config";
+import "dotenv/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    arbitrumSepolia: {
+      url: "https://sepolia-rollup.arbitrum.io/rpc",
+      chainId: 421614,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+  },
+
+  etherscan: {
+    apiKey: {
+      arbitrumSepolia: process.env.ARBISCAN_PRIVATE_KEY || "",
+    },
+    customChains: [
+      {
+        network: "arbitrumSepolia",
+        chainId: 421614,
+        urls: {
+          apiURL: "https://api-sepolia.arbiscan.io/api",
+          browserURL: "https://sepolia.arbiscan.io/",
+        },
+      },
+    ],
+  },
 };
 
 export default config;
--- hardhat/ignition/modules/Bundle.ts
+++ hardhat/ignition/modules/Bundle.ts
@@ -1,9 +1,9 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 import { parseEther } from "viem";
 
-const _initialOwner = "0xbc3f74cecf1fa8270a6fae935e974a5a9570d054";
+const _initialOwner = "0xBC3F74CECF1fA8270A6FAE935e974a5a9570D054";
 
-const BundleModule = buildModule("Bundle", (m) => {
+const BundleModule = buildModule("Bundle", (m: any) => {
   const initialOwner = m.getParameter("initialOwner", _initialOwner);
 
   const bundle = m.contract("Bundle", [initialOwner]);
