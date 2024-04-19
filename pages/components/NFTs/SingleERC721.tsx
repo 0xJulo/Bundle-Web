298bf2782f229dc577ede87a0747e7812784c39a
alerex.eth <alessandromaci96@gmail.com>
2024-04-20 01:04:33 +0200

    add nft flow

--- pages/components/NFTs/SingleERC721.tsx
+++ pages/components/NFTs/SingleERC721.tsx
@@ -1,166 +1,168 @@
-import React from 'react';
-import { Bundle} from '../../utils/ExampleDataStore'
+import React from "react";
+import { Bundle } from "../../utils/ExampleDataStore";
+import abi from "../../utils/CreateNFT.abi.json";
+import { useWriteContract, useAccount } from "wagmi";
 
 interface SingleERC721Props {
-    bundle: Bundle;
+  bundle: Bundle;
 }
 
 const SingleERC721: React.FC<SingleERC721Props> = ({ bundle }) => {
-    const [network, setNetwork] = React.useState<string>('');
-    const [name, setName] = React.useState<string>('');
-    const examplePrice = "0.78ETH";
+  const [name, setName] = React.useState<string>("");
+  const [description, setDescription] = React.useState<string>("");
+  const { address } = useAccount();
+  const { data: hash, error, isPending, writeContract } = useWriteContract();
 
-    // Function for creating the NFT
-    // const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    //     event.preventDefault();
-    //     const formData = new FormData(event.currentTarget);
-    //     const nftData = {
-    //         // const for gettig the data from the form
-    //         network: (formData.get('networkSelect') as string) || '',
-    //         name: (formData.get('nftName') as string) || '',
-    //         description: (formData.get('nftDescription') as string) || '',
-    //         image: (formData.get('nftImageUpload') as string) || '',
-    //     };
-    //     createNFT(nftData);
-    // };
-    return (
-        <section>
-            <div className='mb-8'>
-                <a 
-                    href="https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol" 
-                    target="_blank" rel="noopener noreferrer" 
-                    className='text-blue-500 hover:text-blue-700 underline'>
-                    View OpenZeppelin ERC721 Contract (opens in new tab)
-                </a>
+  const pinTokenURI = async (data: any): Promise<string> => {
+    //setIpfsImageLoading(true);
+    try {
+      const response = await fetch("/api/pinToPinata", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const result = await response.json();
+      const tokenUrl = `https://sapphire-living-peafowl-558.mypinata.cloud/ipfs/${result.IpfsHash}`;
+
+      console.log("Pinned data:", result.IpfsHash);
+      console.log(tokenUrl);
+
+      return tokenUrl; // Return the URL instead of setting state
+    } catch (error) {
+      console.error("Error pinning image to IPFS:", error);
+
+      return Promise.reject(error);
+    }
+  };
+
+  const mint = (tokenURI: string) => {
+    console.log(tokenURI);
+    writeContract({
+      address: "0xb7403174d3325C3aD6B4576E10F85c2b63e68cF8",
+      abi,
+      functionName: "safeMint",
+      args: [address, tokenURI],
+    });
+  };
+
+  React.useEffect(() => {
+    console.log(error);
+  }, [error]);
+
+  // Function for creating the NFT
+  const handleSubmit = async () => {
+    const nftData = {
+      name: name,
+      description: description,
+      image: `https://sapphire-living-peafowl-558.mypinata.cloud/ipfs/QmPHwYuMndJGyroudH6AFH42PzgmWEsEYusw368a3c8gcJ`,
+    };
+
+    const tokenURI = await pinTokenURI(nftData);
+    writeContract({
+      address: "0x7c6F3e985497344cbA9BdFF98d23663f989d47a3",
+      abi,
+      functionName: "safeMint",
+      args: [address, tokenURI],
+    });
+  };
+
+  return (
+    <section>
+      <div className="mb-8">
+        <a
+          href="https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:text-blue-700 underline"
+        >
+          View OpenZeppelin ERC721 Contract (opens in new tab)
+        </a>
+      </div>
+
+      {/* Form for creating the NFT */}
+      <div className="md:w-1/2">
+        {/* NFT name */}
+        <div className="mb-8">
+          <label htmlFor="nftName" className="bundle-text">
+            NFT name
+          </label>
+          <input
+            type="text"
+            name="nftName"
+            id="nftName"
+            className="mt-2 md:mt-4 block w-full h-12 p-2 rounded-md border-[#80BAA8] border-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            placeholder="Enter your NFT name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+
+        {/* NFT description */}
+        <div className="mb-8">
+          <label htmlFor="nftDescription" className="bundle-text">
+            Enter NFT description
+          </label>
+          <textarea
+            id="nftDescription"
+            name="nftDescription"
+            rows={4}
+            className="mt-2 md:mt-4 block w-full p-2 rounded-md border-[#80BAA8] border-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            placeholder="Describe your NFT"
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
+        </div>
+
+        {/* gas cost */}
+        <div className="mb-8">
+          <div>
+            <h3 className="mb-3 bundle-text">Gas cost:</h3>
+            <div>
+              <input
+                type="radio"
+                id="fast"
+                name="gas"
+                value="fast"
+                className="mb-3"
+              />
+              <label htmlFor="fast">High ($0.78)</label>
             </div>
+            <div>
+              <input
+                type="radio"
+                id="middle"
+                name="gas"
+                value="middle"
+                className="mb-3"
+              />
+              <label htmlFor="middle">Average ($0.58)</label>
+            </div>
+            <div>
+              <input type="radio" id="low" name="gas" />
+              <label htmlFor="low">Low ($0.58)</label>
+            </div>
+          </div>
+        </div>
 
-            {/* Form for creating the NFT */}
-            <form className='md:w-1/2'>
-                {/* Network select */}
-                <div className='mb-8'>
-                    <label
-                        htmlFor='networkSelect'
-                        className='bundle-text'
-                    >
-                        Select network
-                    </label>
-                    <select
-                        id='networkSelect'
-                        name='networkSelect'
-                        className='mt-2 md:mt-4 block w-full h-12 p-2 bg-white rounded-md border-[#80BAA8] border-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
-                        onChange={(e) => setNetwork(e.target.value)}
-                    >
-                        <option value=''>
-                            Please select a network
-                        </option>
-                        <option value='ethereum'>Ethereum</option>
-                        <option value='arbitrum'>Arbitrum</option>
-                        <option value='gnosis'>Gnosis Chain</option>
-                    </select>
-                </div>
-
-                {/* NFT name */}
-                <div className='mb-8'>
-                    <label htmlFor='nftName' className='bundle-text'>
-                        NFT name
-                    </label>
-                    <input
-                        type='text'
-                        name='nftName'
-                        id='nftName'
-                        className='mt-2 md:mt-4 block w-full h-12 p-2 rounded-md border-[#80BAA8] border-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
-                        placeholder='Enter your NFT name'
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                </div>
-
-                {/* NFT description */}
-                <div className='mb-8'>
-                    <label
-                        htmlFor='nftDescription'
-                        className='bundle-text'
-                    >
-                        Enter NFT description
-                    </label>
-                    <textarea
-                        id='nftDescription'
-                        name='nftDescription'
-                        rows={4}
-                        className='mt-2 md:mt-4 block w-full p-2 rounded-md border-[#80BAA8] border-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
-                        placeholder='Describe your NFT'
-                    ></textarea>
-                </div>
-
-                {/* NFT image upload */}
-                <div className='mb-8'>
-                    <label
-                        htmlFor='nftImageUpload'
-                        className='bundle-text'
-                    >
-                        Upload your image
-                    </label>
-                    <input
-                        type='file'
-                        name='nftImageUpload'
-                        id='nftImageUpload'
-                        className='mt-2 md:mt-4 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
-                        accept='image/*'
-                    />
-                </div>
-
-                {/* gas cost */}
-                <div className='mb-8'>
-                    <div>
-                        <h3 className='mb-3 bundle-text'>Gas cost:</h3>
-                        <div>
-                            <input
-                                type='radio'
-                                id='fast'
-                                name='gas'
-                                value='fast'
-                                className='mb-3'
-                            />
-                            <label htmlFor='fast'>High ($0.78)</label>
-                        </div>
-                        <div>
-                            <input
-                                type='radio'
-                                id='middle'
-                                name='gas'
-                                value='middle'
-                                className='mb-3'
-                            />
-                            <label htmlFor='middle'>
-                                Average ($0.58)
-                            </label>
-                        </div>
-                        <div>
-                            <input
-                                type='radio'
-                                id='low'
-                                name='gas'
-                            />
-                            <label htmlFor='low'>Low ($0.58)</label>
-                        </div>
-                    </div>
-                </div>
-
-                {/* Submit button */}
-                <button
-                    type='submit'
-                    className='mt-4 px-4 py-2 bg-[#80BAA8] text-white rounded-md hover:bg-[#4c7f6f]'
-                >
-                    {network && name
-                        ? `Mint ${name} to ${
-                                network.charAt(0).toUpperCase() +
-                                network.slice(1)
-                            } for ${examplePrice}`
-                        : 'Mint'}
-                </button>
-            </form>
-        </section>
-    )
-}
+        {/* Submit button */}
+        <button
+          type="button"
+          onClick={() => {
+            handleSubmit();
+          }}
+          className="mt-4 px-4 py-2 bg-[#80BAA8] text-white rounded-md hover:bg-[#4c7f6f]"
+        >
+          {isPending ? "Confirming..." : "Mint"}
+        </button>
+        {hash && <div>Transaction Hash: {hash}</div>}
+      </div>
+    </section>
+  );
+};
 
 export default SingleERC721;
-
