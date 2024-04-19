08581985d546ce2b88adffa90a730d674490c27d
alerex.eth <alessandromaci96@gmail.com>
2024-04-20 00:27:55 +0200

    deploy general create nft contract

---
+++ hardhat/ignition/modules/CreateNFT.ts
@@ -0,0 +1,13 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const _initialOwner = "0xBC3F74CECF1fA8270A6FAE935e974a5a9570D054";
+
+const CreateNFTModule = buildModule("CreateNFT", (m: any) => {
+  const initialOwner = m.getParameter("initialOwner", _initialOwner);
+
+  const createNFT = m.contract("CreateNFT", [initialOwner]);
+
+  return { createNFT };
+});
+
+export default CreateNFTModule;
