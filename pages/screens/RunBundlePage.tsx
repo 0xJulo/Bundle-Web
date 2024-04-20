ed54665ffd9b1553ab7c06e31872e5c842659aae
Julo <9354218+0xJulo@users.noreply.github.com>
2024-04-20 19:51:12 +0100

    vercel fixes

--- pages/screens/RunBundlePage.tsx
+++ pages/screens/RunBundlePage.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { useRouter } from 'next/router';
-import ExampleBundles, { Bundle } from '../utils/ExampleDataStore';
+// import React from 'react';
+// import { useRouter } from 'next/router';
+// import ExampleBundles, { Bundle } from '../utils/ExampleDataStore';
 
-const RunBundleScreen: React.FC = () => {
-  const router = useRouter();
-  const { bundleId } = router.query;
+// const RunBundleScreen: React.FC = () => {
+//   const router = useRouter();
+//   const { bundleId } = router.query;
 
-  const bundleIdNum = typeof bundleId === 'string' ? Number(bundleId) : -1;
-  const bundle = ExampleBundles.find((bundle) => bundle.id === bundleIdNum);
+//   const bundleIdNum = typeof bundleId === 'string' ? Number(bundleId) : -1;
+//   const bundle = ExampleBundles.find((bundle) => bundle.id === bundleIdNum);
 
-  if (!bundle) {
-    return <div>Bundle not found</div>;
-  };
+//   if (!bundle) {
+//     return <div>Bundle not found</div>;
+//   };
 
-  return (
-    <div>
-        <div>{bundle.name}</div>
-        <div>{bundle.description}</div>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//         <div>{bundle.name}</div>
+//         <div>{bundle.description}</div>
+//     </div>
+//   );
+// };
 
-export default RunBundleScreen;
+// export default RunBundleScreen;
 
